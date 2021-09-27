d6da2ba9ba94bd9237ece214f094cec0b609c7f9
likunming1 <likunming1@longfor.com>
2021-09-27 13:20:03 +0800

    两数之和

--- Algo-Swift/03Array-两数之和.swift
+++ Algo-Swift/03Array-两数之和.swift
@@ -26,6 +26,20 @@
  
  
  */
-func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    
+func twoSumSolution(_ nums: [Int], _ target: Int) -> [Int] {
+    //使用字典保存,key 是我们期待的值,value 是数组的索引
+    //当数组中存在我们期待值 expect 对应的索引时,返回对应索引所在的位置fromIndex
+    // 返回数组[fromIndex,index]
+    var dic: [Int: Int] = [:]
+    for (index, num) in nums.enumerated() {
+        //先通过遍历找我们的期望值
+        let expect = target - num
+        // 如果数组中存在字典value 对应的索引
+        if let fromIndex = dic[expect], fromIndex != index {
+            return [fromIndex, index]
+        }
+        //使用字典保存索引
+        dic[num] = index
+    }
+    return []
 }
